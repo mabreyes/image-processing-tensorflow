08cd80c1d31d2c93c6a46d9a670948ac59232b82
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2017-09-16 01:12:40 +0800

    Implement a negative gradient of the cost function

--- session_2/gradient_descent.py
+++ session_2/gradient_descent.py
@@ -22,4 +22,17 @@ cost = np.sin(hz*x)*np.exp(-x)
 ax.plot(x, cost)
 ax.set_ylabel('Cost')
 ax.set_xlabel('Some Parameter')
+# Get the difference between every value
+gradient = np.diff(cost)
+n_iterations = 500
+cmap = plt.get_cmap('coolwarm')
+c_norm = colors.Normalize(vmin=0, vmax=n_iterations)
+scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=cmap)
+init_p = np.random.randint(len(x)*0.2, len(x)*0.8) # 120
+learning_rate = 1.0
+# Implement a negative gradient of the function
+for iter_i in range(n_iterations):
+	init_p -=learning_rate * gradient[int(init_p)]
+	ax.plot(x[int(init_p)], cost[int(init_p)], 'ro', alpha=(iter_i + 1) / n_iterations, color=scalar_map.to_rgba(iter_i))
+	ax.plot(x[int(init_p)])
 plt.show()
