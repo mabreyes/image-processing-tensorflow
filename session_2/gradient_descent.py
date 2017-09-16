21acb7ee7db5d3702ae7ed2d07990e7061fee16b
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2017-09-17 01:02:21 +0800

    Implement two-parameter gradient descent

--- session_2/gradient_descent.py
+++ session_2/gradient_descent.py
@@ -1,3 +1,5 @@
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
 import tensorflow as tf
 import numpy as np 
 import matplotlib.pyplot as plt 
@@ -9,7 +11,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 plt.style.use('ggplot')
 
-# TODO: Understand the concepts especially cost function and get response from the forum
+# Single parameter gradient descent
+# TODO: Search as to why the iteration stops at the nearest local minima
 """ Local minima/optima 
 Gradient descent functions contain "minima" which is the lowest value
 or point and maxima or the highest value. Local minima/maxima means the 
@@ -30,15 +33,30 @@ n_iterations = 500
 cmap = plt.get_cmap('coolwarm')
 c_norm = colors.Normalize(vmin=0, vmax=n_iterations)
 scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=cmap)
-""" init_p means the location of the nth(init_p) number in the linear space, 
-down to the minima (if the gradient is negative)
+""" init_p means the location of the nth(init_p) number in the linear space or
+where should the point start, down to the nearest local minima (if the gradient 
+is negative)
 """
 init_p = 120 # np.random.randint(len(x)*0.2, len(x)*0.8)
+# Means how the iteration should move forward
 learning_rate = 1.0
 # Implement a negative gradient of the function
 for iter_i in range(n_iterations):
 	# The dots must go down since it is decremented
 	init_p -=learning_rate * gradient[int(init_p)]
+	# Parameters x, y, circle color, transparency, color
 	ax.plot(x[int(init_p)], cost[int(init_p)], 'ro', alpha=(iter_i + 1) / n_iterations, color=scalar_map.to_rgba(iter_i))
 	ax.plot(x[int(init_p)])
+plt.show()
+
+# Two-parameter gradient descent
+# TODO: Understand this concept
+fig = plt.figure(figsize=(10, 6))
+ax = fig.gca(projection='3d')
+x, y = np.mgrid[-1:1:0.02, -1:1:0.02]
+X, Y, Z = x, y, np.sin(hz*x)*np.exp(-x)*np.cos(hz*y)*np.exp(-y)
+ax.plot_surface(X, Y, Z, rstride=2, cstride=2, alpha=0.75, cmap='jet', shade=False)
+ax.set_ylabel('Some Parameter 2')
+ax.set_xlabel('Some Parameter 1')
+ax.set_zlabel('Cost')
 plt.show()
